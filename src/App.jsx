b6d75d4ebb5e0ec5534fa7b1894d44a3be0f79c7
John O'Brien <johnobrien.dev@gmail.com>
2024-04-08 22:33:10 -0400

    add nav to header for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
-
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import About from './components/About';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>
+              <Header />
+              <main>
+                <Homepage />
+              </main>
+              <Footer />
+            </div>,
+    index: true, // Marks this as the default route
+  },
+  {
+    path: "/about",
+    element: <div>
+              <Header />
+              <main>
+                <About />
+              </main>
+              <Footer />
+            </div>,
+  },
+  {
+    path: "/contact",
+    element: <div>
+              <Header />
+              <main>
+                <Contact />
+              </main>
+              <Footer />
+            </div>,
+  },
+]);
 
 function App() {
-  
-  
+  return (
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
+

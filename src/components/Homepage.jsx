f83f1d1267fd7d6d406fad9120a07b40c80923c7
John O'Brien <johnobrien.dev@gmail.com>
2024-04-11 13:39:44 -0400

    add sample text to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,8 @@
 export default function Homepage(){
     return(
+        <>
         <h1 className="bg-slate-500">Homepage</h1>
+        <p className="p-5 text-blue-500">This is the homepage</p>
+        </>
     )
 }

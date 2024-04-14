61122f7f10983b8090051b91ea25e86a52ab187c
John O'Brien <johnobrien.dev@gmail.com>
2024-04-14 17:27:55 -0400

    change navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 export default function Header() {
     return(
-        <header>
-            <nav className="flex items-center">
+        <header className="shadow-md w-full fixed top-0 left-0">
+            <nav className="md:flex bg-white py-4 items-center">
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
                 <Link to='/contact'>Contact</Link>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,9 @@
 export default function Homepage(){
     return(
-        <>
+        <div>
         <h1 className="bg-slate-500">John O'Brien</h1>
         <img src="john_beret.jpg" className="h-[500px] w-auto" alt="" />
         <p className="p-5 text-blue-500">This is the homepage</p>
-        </>
+        </div>
     )
 }
